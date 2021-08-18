dbba7048e935428389ab846b7a1a3dea521203e2
Anton Berg <anton@os3d.se>
2021-08-18 17:16:57 +0200

    update startmax

--- startMax.ps1
+++ startMax.ps1
@@ -3,4 +3,7 @@ if (Test-Connection -TargetName comseelm-nt9100.ikea.com -Quiet) {
     & ".\venv_3dsmax\Scripts\activate" /run 
     Write-Host "start max with this virtualEnv active" 
     & "C:\Program Files\Autodesk\3ds Max 2021\3dsmax.exe" /run 
+} else {
+    Write-Host "cannot activate since you have not started Cisco VPN connection"
+
 }
