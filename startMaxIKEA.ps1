ad857efc3845d2695963b68cada1b958b2485bbc
Anton Berg <anton@os3d.se>
2021-08-19 07:54:59 +0200

    renamed startMax to startMaxIKEA since it is VPN specific.. need another version for comp. outside the VPN walls

---
+++ startMaxIKEA.ps1
@@ -0,0 +1,9 @@
+if (Test-Connection -TargetName comseelm-nt9100.ikea.com -Quiet) {
+    Write-Host "activate venv_3dsmax" 
+    & ".\venv_3dsmax\Scripts\activate" /run 
+    Write-Host "start max with this virtualEnv active" 
+    & "C:\Program Files\Autodesk\3ds Max 2021\3dsmax.exe" /run 
+} else {
+    Write-Host "cannot activate since you have not started Cisco VPN connection"
+
+}
